9b59c26e62daa466b80685b9d1e55fff8716c457
balajicrnw <balaji.cr@nativewit.in>
2026-02-16 15:59:20 +0530

    first commit

--- dart_practice/OOPS/Encapsulation.dart
+++ dart_practice/OOPS/Encapsulation.dart
@@ -1,8 +1,17 @@
 class BankAccount{
+  String? accountNumber;
+  String? accountHolder;
+  String? accountpin;
   double? _balance;
 
-  void getBalance(){
+  BankAccount(this.accountNumber,this.accountHolder,this.accountpin,this._balance);
+  void getBalance(String name,String pin){
+    if(name==accountHolder && pin==accountpin){
     print("Current balance: $_balance");
+  }
+  else{
+    print("Invalid credentials");
+  }
   }
 
   void setBalance(double? newBalance){
@@ -16,11 +25,11 @@ class BankAccount{
 }
 
 void main(){
-  BankAccount b1=new BankAccount();
-  b1.getBalance();
+  BankAccount b1=new BankAccount("123456789","Balaji","1234",500.0);
+  b1.getBalance("Balaji","1234");
   b1.setBalance(1000.0);
-  b1.getBalance();
+  b1.getBalance("Balaji","1234");
   b1.deposit(500.0);
-  b1.getBalance();
+  b1.getBalance("Balaji","1234");
 
 }
